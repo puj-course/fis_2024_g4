57e28a373ed823f8c194bef7ada938c49bd6cfc0
Urbinabina <125985060+Urbinabina@users.noreply.github.com>
2024-11-07 01:28:25 -0500

    Update bd.py

--- src/main/bd.py
+++ src/main/bd.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://aiurbinamox:123@proyecto.1lqlm.mongodb.net/')
+client = MongoClient('mongodb+srv://aiurbinamox:123@proyecto.1lqlm.mongodb.net/', ssl=False)
 db = client['Proyecto']  
 pokemon_collection = db['Pokemon'] 
 usuarios_collection = db['Usuarios']
